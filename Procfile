43097a0c09c2947cdbab5ffcd91b53e80836f7f6
Arti Shelar <arti.shelar@st.ovgu.de>
2021-01-12 21:50:51 +0100

    heroku setup

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar webapp-runner.jar --expand-war target/*.war
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar webapp-runner.jar --expand-war target/wikisearcher-0.0.1-SNAPSHOT.war
--- src/main/java/com/ovgu/informationretrieval/wikisearcher/WikisearcherApplication.java
+++ src/main/java/com/ovgu/informationretrieval/wikisearcher/WikisearcherApplication.java
@@ -3,14 +3,16 @@ package com.ovgu.informationretrieval.wikisearcher;
 import com.ovgu.informationretrieval.wikisearcher.core.lucene.Indexer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 
 @SpringBootApplication
-public class WikisearcherApplication {
+public class WikisearcherApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		System.out.println("Application Started");
 		//SpringApplication.run(WikisearcherApplication.class, args);
 		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(WikisearcherApplication.class);
 		ApplicationInitializer appService = configurableApplicationContext.getBean(ApplicationInitializer.class);
@@ -18,7 +20,7 @@ public class WikisearcherApplication {
 			Indexer indexer = configurableApplicationContext.getBean(Indexer.class);
 			indexer.index();
 		} catch (IOException e) { e.printStackTrace(); }
-		System.out.println("Application Started");
+
 	}
 
 }
