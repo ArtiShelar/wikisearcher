fb789a08d59ec9a75e7bccfb667c48b0fb0cb815
Arti Shelar <arti.shelar@st.ovgu.de>
2021-01-13 13:55:31 +0100

    code update and frontend folder creation

--- src/main/java/com/ovgu/informationretrieval/wikisearcher/ServletInitializer.java
+++ src/main/java/com/ovgu/informationretrieval/wikisearcher/ServletInitializer.java
@@ -1,12 +1,28 @@
 package com.ovgu.informationretrieval.wikisearcher;
 
+import com.ovgu.informationretrieval.wikisearcher.core.lucene.Indexer;
+import org.springframework.boot.ApplicationContextFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("ARTI : starting application");
+		application.sources(WikisearcherApplication.class);
+		//application.contextFactory(ApplicationContextFactory.DEFAULT);
+		ConfigurableApplicationContext configurableApplicationContext = application.run();
+		System.out.println("ConfigurableApplicationContext :: "+configurableApplicationContext);
+		ApplicationInitializer appService = configurableApplicationContext.getBean(ApplicationInitializer.class);
+		try {
+			Indexer indexer = configurableApplicationContext.getBean(Indexer.class);
+			indexer.index();
+		} catch (IOException e) { e.printStackTrace(); }
+		System.out.println("ARTI : application started");
 		return application.sources(WikisearcherApplication.class);
 	}
 
--- src/main/java/com/ovgu/informationretrieval/wikisearcher/WikisearcherApplication.java
+++ src/main/java/com/ovgu/informationretrieval/wikisearcher/WikisearcherApplication.java
@@ -3,24 +3,35 @@ package com.ovgu.informationretrieval.wikisearcher;
 import com.ovgu.informationretrieval.wikisearcher.core.lucene.Indexer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 
 @SpringBootApplication
-public class WikisearcherApplication extends SpringBootServletInitializer {
+public class WikisearcherApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Application Started");
-		//SpringApplication.run(WikisearcherApplication.class, args);
+//		SpringApplication application = new SpringApplication(WikisearcherApplication.class);
+//		initHooks(application);
+//		application.run(args);
+		System.out.println("ARTI 2 : application starting");
 		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(WikisearcherApplication.class);
 		ApplicationInitializer appService = configurableApplicationContext.getBean(ApplicationInitializer.class);
 		try {
 			Indexer indexer = configurableApplicationContext.getBean(Indexer.class);
 			indexer.index();
 		} catch (IOException e) { e.printStackTrace(); }
-
 	}
-
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		WikisearcherApplication.initHooks(builder.application());
+//		return builder.sources(WikisearcherApplication.class);
+//	}
+//
+//	static void initHooks(SpringApplication application) {
+//		System.out.println("Application initialization started");
+//	}
 }
